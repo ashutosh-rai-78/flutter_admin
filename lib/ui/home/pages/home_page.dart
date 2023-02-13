22c862be5cd1686dc3cafa465ccebcaf0e114910
ashutosh-rai-78 <ashutosh.developer78@gmail.com>
2023-02-13 13:19:27 +0530

    Responsive Scaffold Widget Created

--- lib/ui/home/pages/home_page.dart
+++ lib/ui/home/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_admin/ui/Product_page/pages/products_page.dart';
 import 'package:flutter_admin/ui/home/view_models/home_view_model.dart';
 import 'package:flutter_admin/ui/home/widgets/bottom_navigation.dart';
 import 'package:flutter_admin/ui/home/widgets/custom_navigation_rail.dart';
+import 'package:flutter_admin/ui/home/widgets/responsive_scaffold.dart';
 import 'package:go_router/go_router.dart';
 import 'package:view_model_x/view_model_x.dart';
 
@@ -50,24 +51,58 @@ class HomePage extends StatelessWidget with RouteWrapper {
   @override
   Widget build(BuildContext context) {
     debugPrint("ghghjdg");
-    var width = MediaQuery.of(context).size.width;
     /* int currentPageIndex =
         context.vm<HomeViewModel>().indexStateFlow.watch(context); */
     // debugPrint("Current Page Index : $currentPageIndex ");
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        toolbarHeight: 50,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(2.0),
+    return ResponsiveScaffold(
+      appBartitle: const Text("Admin App"),
+      navigationRail: (context, extended) =>
+          CustomNavigationRail(extended: extended),
+      actions: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
           child: Container(
-            color: Theme.of(context).colorScheme.outline,
-            height: 2,
-          ),
+              padding: const EdgeInsets.all(5),
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              child: const Icon(Icons.person)),
         ),
-        title: const Text("Flutter Admin"),
-        actions: [
+        PopupMenuButton<String>(
+          icon: const Icon(Icons.keyboard_arrow_down_outlined),
+          onSelected: (value) {
+            switch (value) {
+              case 'Logout':
+                context.vm<HomeViewModel>().logout().then((value) {
+                  context.pushReplacementNamed(Routes.login.name);
+                }).onError((error, stackTrace) {});
+                break;
+              case 'Settings':
+                break;
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return {'Logout', 'Settings'}.map((String choice) {
+              return PopupMenuItem<String>(
+                value: choice,
+                child: Text(choice),
+              );
+            }).toList();
+          },
+        ),
+      ],
+      bottomNavigationBar: const BottomNavigation(),
+      body: StateFlowBuilder(
+        builder: (context, currrentIndex) => screen(context, currrentIndex),
+        stateFlow: context.vm<HomeViewModel>().indexStateFlow,
+      ),
+    );
+  }
+}
+
+
+/* 
+
+[
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: Container(
@@ -98,20 +133,4 @@ class HomePage extends StatelessWidget with RouteWrapper {
             },
           ),
         ],
-      ),
-      bottomNavigationBar: width < 650 ? const BottomNavigation() : null,
-      body: Row(
-        children: [
-          if (width >= 650) CustomNavigationRail(extended: width >= 900),
-          Expanded(
-              child: StateFlowBuilder(
-            builder: (context, currrentIndex) {
-              return screen(context, currrentIndex);
-            },
-            stateFlow: context.vm<HomeViewModel>().indexStateFlow,
-          )),
-        ],
-      ),
-    );
-  }
-}
+ */
